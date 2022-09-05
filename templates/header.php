1c00242d00c3300b74ca0e6907cf7c9dc5892238
Aurélie Frémaux <aurelie.fremaux02@gmail.com>
2022-09-05 12:48:06 +0200

    ajout du header et footer

---
+++ templates/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Literie 3000 
+    </title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+    <div class="container">
+        <div class="logo-wrapper">
+        <img src="img/logo.png" alt="Literie 3000" class="logo">
+        
+        </div>
+    </div>  
